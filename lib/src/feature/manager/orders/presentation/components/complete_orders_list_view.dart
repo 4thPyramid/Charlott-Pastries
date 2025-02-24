7be3cd7274443f4013fceb7c39dceb016db80a33
Ahmed Slman <ahmedslman151@gmail.com>
2025-02-24 14:19:46 +0200

    Refactor order components and models: update UI layout, make flower price optional, and integrate date filtering in newest orders view

--- lib/src/feature/manager/orders/presentation/components/complete_orders_list_view.dart
+++ lib/src/feature/manager/orders/presentation/components/complete_orders_list_view.dart
@@ -1,5 +1,9 @@
+import 'package:charlot/core/app_cubit/app_cubit.dart';
+import 'package:charlot/core/common/widgets/custom_date_filter.dart';
 import 'package:charlot/core/common/widgets/shared_order_item_card.dart';
 import 'package:charlot/core/services/service_locator.dart';
+import 'package:charlot/core/theme/app_colors.dart';
+import 'package:charlot/core/utils/app_styles.dart';
 import 'package:charlot/src/feature/manager/orders/presentation/logic/completed/completed_order_cubit.dart';
 import 'package:charlot/src/feature/manager/orders/presentation/logic/completed/completed_order_state.dart';
 import 'package:flutter/material.dart';
@@ -20,7 +24,7 @@ class _ManagerCompleteOrdersListViewState
   @override
   void initState() {
     _cubit = getIt<CompletedOrdersCubit>();
-    _cubit.call(null, null); // Initial call with no filters
+    _cubit.call(null, null);
     super.initState();
   }
 
@@ -38,7 +42,7 @@ class _ManagerCompleteOrdersListViewState
         children: [
           CustomDateFilter(
             onFilterApplied: (startDate, endDate) {
-              _cubit.call(startDate, endDate); // Call Cubit with selected dates
+              _cubit.call(startDate, endDate);
             },
           ),
           Expanded(
@@ -71,110 +75,5 @@ class _ManagerCompleteOrdersListViewState
         ],
       ),
     );
-  }
-}
-
-class CustomDateFilter extends StatefulWidget {
-  final DateTime? initialStartDate;
-  final DateTime? initialEndDate;
-  final Function(DateTime?, DateTime?) onFilterApplied;
-
-  const CustomDateFilter({
-    super.key,
-    this.initialStartDate,
-    this.initialEndDate,
-    required this.onFilterApplied,
-  });
-
-  @override
-  State<CustomDateFilter> createState() => _CustomDateFilterState();
-}
-
-class _CustomDateFilterState extends State<CustomDateFilter> {
-  DateTime? startDate;
-  DateTime? endDate;
-
-  @override
-  void initState() {
-    startDate = widget.initialStartDate;
-    endDate = widget.initialEndDate;
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              ElevatedButton.icon(
-                icon: const Icon(Icons.calendar_today),
-                label: Text(
-                  startDate != null
-                      ? '${startDate!.day}/${startDate!.month}/${startDate!.year}'
-                      : 'اختر تاريخ البداية',
-                ),
-                onPressed: () async => await _selectDate(true),
-              ),
-              ElevatedButton.icon(
-                icon: const Icon(Icons.calendar_today),
-                label: Text(
-                  endDate != null
-                      ? '${endDate!.day}/${endDate!.month}/${endDate!.year}'
-                      : 'اختر تاريخ النهاية',
-                ),
-                onPressed: () async => await _selectDate(false),
-              ),
-            ],
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.blue,
-              foregroundColor: Colors.white,
-            ),
-            onPressed: _applyFilter,
-            child: const Text('تطبيق التصفية'),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Future<void> _selectDate(bool isStartDate) async {
-    final pickedDate = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2000),
-      lastDate: DateTime.now(),
-      helpText: isStartDate ? 'اختر تاريخ البداية' : 'اختر تاريخ النهاية',
-    );
-
-    if (pickedDate != null) {
-      setState(() {
-        if (isStartDate) {
-          startDate = pickedDate;
-          if (endDate != null && startDate!.isAfter(endDate!)) {
-            endDate = null; // Reset end date if it’s before the new start date
-          }
-        } else {
-          endDate = pickedDate;
-        }
-      });
-    }
-  }
-
-  void _applyFilter() {
-    if (startDate != null && endDate != null && startDate!.isAfter(endDate!)) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text('تاريخ البداية يجب أن يكون قبل تاريخ النهاية'),
-        ),
-      );
-      return;
-    }
-    widget.onFilterApplied(startDate, endDate);
   }
 }
