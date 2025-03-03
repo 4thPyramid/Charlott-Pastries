f5d39c84bc4fe266edd08dd494f1cb2245909013
Ahmed Slman <ahmedslman151@gmail.com>
2025-03-03 15:34:30 +0200

    Update .gitignore and add CMake configuration files for various architectures

--- lib/src/feature/orderDetails/presentation/components/order_details_image_header.dart
+++ lib/src/feature/orderDetails/presentation/components/order_details_image_header.dart
@@ -1,6 +1,10 @@
-import 'package:charlot/src/feature/orderDetails/presentation/components/order_images_list_view.dart';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import '../../data/model/order_details_response.dart';
 
@@ -21,6 +25,72 @@ class _OrderDetailsImageHeaderState extends State<OrderDetailsImageHeader> {
   void dispose() {
     _pageController.dispose();
     super.dispose();
+  }
+
+  Future<void> _requestPermissions() async {
+    if (Platform.isAndroid) {
+      Map<Permission, PermissionStatus> statuses = await [
+        Permission.storage,
+      ].request();
+
+      if (statuses[Permission.storage]!.isGranted) {
+        print("تم منح الإذن على Android");
+      } else {
+        print("تم رفض الإذن على Android");
+        if (statuses[Permission.storage]!.isPermanentlyDenied) {
+          openAppSettings();
+        }
+      }
+    } else if (Platform.isIOS) {
+      Map<Permission, PermissionStatus> statuses = await [
+        Permission.photos,
+        Permission.photosAddOnly,
+      ].request();
+
+      if (statuses[Permission.photos]!.isGranted ||
+          statuses[Permission.photosAddOnly]!.isGranted) {
+        print("تم منح الإذن على iOS");
+      } else {
+        print("تم رفض الإذن على iOS");
+        if (statuses[Permission.photos]!.isPermanentlyDenied) {
+          openAppSettings();
+        }
+      }
+    }
+  }
+
+  Future<void> _downloadCurrentImage() async {
+    final imageUrl = widget.images[_currentPage].image;
+
+    Directory? directory;
+
+    if (Platform.isAndroid) {
+      directory = await getExternalStorageDirectory();
+    } else if (Platform.isIOS) {
+      directory =
+          await getApplicationDocumentsDirectory(); // iOS لا يدعم التخزين الخارجي
+    }
+
+    final savedDir = directory?.path;
+
+    if (savedDir == null) {
+      print("فشل في الحصول على مسار التخزين");
+      return;
+    }
+
+    try {
+      final taskId = await FlutterDownloader.enqueue(
+        url: imageUrl,
+        savedDir: savedDir,
+        showNotification: true,
+        openFileFromNotification: true,
+        saveInPublicStorage: Platform.isAndroid, // تجنب استخدامه على iOS
+      );
+
+      print("تم بدء التنزيل، Task ID: $taskId");
+    } catch (e) {
+      print("حدث خطأ أثناء التنزيل: $e");
+    }
   }
 
   @override
@@ -59,20 +129,10 @@ class _OrderDetailsImageHeaderState extends State<OrderDetailsImageHeader> {
               child: IconButton(
                 icon: const Icon(Icons.download_rounded,
                     color: Colors.white, size: 30),
-                onPressed: () {
-                  // ImageDownloader()
-                  //     .downloadImage(widget.images[_currentPage].image);
+                onPressed: () async {
+                  await _requestPermissions();
+                  await _downloadCurrentImage();
                 },
-              ),
-            ),
-            Positioned(
-              left: 10.w,
-              right: 10.w,
-              bottom: 20,
-              child: OrderImagesListView(
-                images: widget.images,
-                pageController: _pageController,
-                currentPage: _currentPage,
               ),
             ),
           ],
